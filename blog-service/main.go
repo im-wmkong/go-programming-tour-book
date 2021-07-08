2fb221e29ea38d382691c6946cfe42b88954a39d
WmKong <im.wmkong@gmail.com>
2021-07-08 23:34:12 +0800

    blag add router

---
+++ blog-service/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-programming-tour-book/blog-service/internal/routers"
+)
+
+func main() {
+	router := routers.NewRouter()
+
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}
