a000db41ab04f6cbd97b8156e376b8e0603bdff4
WmKong <im.wmkong@gmail.com>
2021-07-09 23:59:50 +0800

    blag add db and logger

---
+++ blog-service/global/setting.go
@@ -0,0 +1,9 @@
+package global
+
+import "github.com/go-programming-tour-book/blog-service/pkg/setting"
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSertting     *setting.AppSerttingS
+	DatabaseSetting *setting.DatabaseSettingS
+)
