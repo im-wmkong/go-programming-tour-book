8b73bb7903345010d8adaea9297e11b446bbff79
WmKong <im.wmkong@gmail.com>
2021-07-11 17:45:55 +0800

    blag response

--- blog-service/internal/routers/router.go
+++ blog-service/internal/routers/router.go
@@ -2,7 +2,7 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/go-programming-tour-book/blog-service/internal/routers/api/v1"
+	v1 "github.com/go-programming-tour-book/blog-service/internal/routers/api/v1"
 )
 
 func NewRouter() *gin.Engine {
@@ -18,12 +18,14 @@ func NewRouter() *gin.Engine {
 		apiv1.PUT("/tags/:id", tag.Update)
 		apiv1.PATCH("/tags/:id/state", tag.Update)
 		apiv1.GET("/tags", tag.List)
+		apiv1.GET("/tags/:id", tag.Get)
 
 		apiv1.POST("/articles", article.Create)
 		apiv1.DELETE("/articles/:id", article.Delete)
 		apiv1.PUT("/articles/:id", article.Update)
 		apiv1.PATCH("/articles/:id/state", article.Update)
 		apiv1.GET("/articles", article.List)
+		apiv1.GET("/articles/:id", article.Get)
 	}
 
 	return r
---
+++ blog-service/pkg/app/pagination.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-programming-tour-book/blog-service/global"
+	"github.com/go-programming-tour-book/blog-service/pkg/convert"
+)
+
+func GetPage(c *gin.Context) int {
+	page := convert.StrTo(c.Query("page")).MustInt()
+	if page <= 0 {
+		return 1
+	}
+	return page
+}
+
+func GetPageSize(c *gin.Context) int {
+	pageSize := convert.StrTo(c.Query("page_size")).MustInt()
+	if pageSize <= 0 {
+		return global.AppSertting.DefaultPageSize
+	}
+	if pageSize > global.AppSertting.MaxPageSize {
+		return global.AppSertting.MaxPageSize
+	}
+	return pageSize
+}
+
+func GetPageOffset(page, pageSize int) int {
+	result := 0
+	if page > 0 {
+		result = (page - 1) * pageSize
+	}
+	return result
+}
