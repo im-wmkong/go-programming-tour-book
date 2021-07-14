78c026be81c0f55ffb97df3852c56a621bfd3eb8
WmKong <im.wmkong@gmail.com>
2021-07-14 22:55:15 +0800

    blog add request

--- blog-service/internal/service/tag.go
+++ blog-service/internal/service/tag.go
@@ -2,52 +2,26 @@ package service
 
 import (
 	"github.com/go-programming-tour-book/blog-service/internal/model"
+	"github.com/go-programming-tour-book/blog-service/internal/request"
 	"github.com/go-programming-tour-book/blog-service/pkg/app"
 )
 
-type CountTagRequest struct {
-	Name  string `form:"name" binding:"max=100"`
-	State uint8  `form:"state,default=1" binding:"oneof=0 1"`
-}
-
-type TagListRequest struct {
-	Name  string `form:"name" binding:"max=100"`
-	State uint8  `form:"state,default=1" binding:"oneof=0 1"`
-}
-
-type CreateTagRequest struct {
-	Name      string `form:"name" binding:"required,min=3,max=100"`
-	CreatedBy string `form:"created_by" binding:"required,min=3,max=100"`
-	State     uint8  `form:"state,default=1" binding:"oneof=0 1"`
-}
-
-type UpdateTagRequest struct {
-	ID        uint32 `form:"id" binding:"required,gte=1"`
-	Name      string `form:"name" binding:"min=3,max=100"`
-	State     uint8  `form:"state" binding:"required,oneof=0 1"`
-	UpdatedBy string `form:"updated_by" binding:"required,min=3,max=100"`
-}
-
-type DeleteTagRequest struct {
-	ID uint32 `form:"id" binding:"required,gte=1"`
-}
-
-func (s *Service) CountTag(param *CountTagRequest) (int, error) {
+func (s *Service) CountTag(param *request.CountTagRequest) (int, error) {
 	return s.dao.CountTag(param.Name, param.State)
 }
 
-func (s *Service) GetTagList(param *TagListRequest, pager *app.Pager) ([]*model.Tag, error) {
+func (s *Service) GetTagList(param *request.TagListRequest, pager *app.Pager) ([]*model.Tag, error) {
 	return s.dao.GetTagList(param.Name, param.State, pager.Page, pager.PageSize)
 }
 
-func (s *Service) CreateTag(param *CreateTagRequest) error {
+func (s *Service) CreateTag(param *request.CreateTagRequest) error {
 	return s.dao.CreateTag(param.Name, param.State, param.CreatedBy)
 }
 
-func (s *Service) UpdateTag(param *UpdateTagRequest) error {
+func (s *Service) UpdateTag(param *request.UpdateTagRequest) error {
 	return s.dao.UpdateTag(param.ID, param.Name, param.State, param.UpdatedBy)
 }
 
-func (s *Service) DeleteTag(param *DeleteTagRequest) error {
+func (s *Service) DeleteTag(param *request.DeleteTagRequest) error {
 	return s.dao.DeleteTag(param.ID)
 }
